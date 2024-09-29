dd43fd41c8a97af960f474dd56241440ba298fb3
seabee <3200104392@zju.edu.cn>
2024-09-29 11:13:28 +0800

    add: AI HW_3 Fin

--- 人工智能算法与系统/Projects/HW_3/README.md
+++ 人工智能算法与系统/Projects/HW_3/README.md
@@ -1,15 +1,31 @@
-- 笑死，在 GitHub 上找到别人的 [src](https://github.com/QikaiXu/Robot-Maze-Solving) 了
+# README
 
-    注意区分：24 年要求继承 `QRobot`，这位直接继承了 `TorchRobot` 在写
+## Reference
+
+笑死，在 GitHub 上找到别人的 [src](https://github.com/QikaiXu/Robot-Maze-Solving) 了
+
+- 注意区分：24 年要求继承 `QRobot`，这位直接继承了 `TorchRobot` 在写
 
     => which means 我们需要参照 `TorchRobot` 的实现自己再搓一些成员函数
 
-- 实际上提交的代码在 `main.py`，其中仅包含 DFS 的部分 + “自己” 实现的 DQN
+---
 
-    - 不知道为啥用 `mine: bool` 控制 QNetwork 的隐藏层在 OJ 上总是报错
+- 虽然看到迟了，但 [Fazzie 前辈](https://fazzie-key.cool/2021/06/15/%E6%9C%BA%E5%99%A8%E4%BA%BA%E8%87%AA%E5%8A%A8%E8%B5%B0%E8%BF%B7%E5%AE%AB/#QRobot-%E7%B1%BB) 的 blog 也是极好的
 
-        => 最后还是用了默认的 `(512, 512,)`
+    => 人家甚至附带了 Report
 
-    - OJ 上给的 training epoch 对于本垃圾 bot 来说明显不足
+- [Y-vic 前辈](https://github.com/Y-vic/ZJU_AI_ML_Lab/tree/master) 的 GitHub 仓库
 
-        => 参照了最上面的 repo，在 `__init__()` 的时候自己先卷完
+    包含了 23 年的 4 个 Lab 代码（但只有 “机器人自动走迷宫” 和本次 match）
+
+## Files
+
+实际上提交的代码在 `main.py`，其中仅包含 DFS 的部分 + “自己” 实现的 DQN
+
+- 不知道为啥用 `mine: bool` 控制 QNetwork 的隐藏层在 OJ 上总是报错
+
+    => 最后还是用了默认的 `(512, 512,)`
+
+- OJ 上给的 training epoch 对于本垃圾 bot 来说明显不足
+
+    => 参照了最上面的 repo，在 `__init__()` 的时候自己先卷完
--- 人工智能算法与系统/Projects/HW_3/main.py
+++ 人工智能算法与系统/Projects/HW_3/main.py
@@ -4,106 +4,18 @@
 import os
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torch import optim
+import torch.nn.functional as F 
+
 from Maze import Maze
 from Runner import Runner
 from QRobot import QRobot
 from ReplayDataSet import ReplayDataSet
 from torch_py.QNetwork import QNetwork
 from torch_py.MinDQNRobot import MinDQNRobot as TorchRobot # PyTorch版本
-# from keras_py.MinDQNRobot import MinDQNRobot as KerasRobot # Keras版本
-import matplotlib.pyplot as plt
-
-import torch
-from torch import  optim
-import torch.nn.functional as F 
-
-class SearchTree(object):
-
-
-    def __init__(self, loc=(), action='', parent=None):
-        """
-        初始化搜索树节点对象
-        :param loc: 新节点的机器人所处位置
-        :param action: 新节点的对应的移动方向
-        :param parent: 新节点的父辈节点
-        """
-
-        self.loc = loc  # 当前节点位置
-        self.to_this_action = action  # 到达当前节点的动作
-        self.parent = parent  # 当前节点的父节点
-        self.children = []  # 当前节点的子节点
-
-    def add_child(self, child):
-        """
-        添加子节点
-        :param child:待添加的子节点
-        """
-        self.children.append(child)
-
-    def is_leaf(self):
-        """
-        判断当前节点是否是叶子节点
-        """
-        return len(self.children) == 0
-
-def expand(maze, is_visit_m, node):
-    """
-    拓展叶子节点，即为当前的叶子节点添加执行合法动作后到达的子节点
-    :param maze: 迷宫对象
-    :param is_visit_m: 记录迷宫每个位置是否访问的矩阵
-    :param node: 待拓展的叶子节点
-    """
-    can_move = maze.can_move_actions(node.loc)
-    for a in can_move:
-        new_loc = tuple(node.loc[i] + move_map[a][i] for i in range(2))
-        if not is_visit_m[new_loc]:
-            child = SearchTree(loc=new_loc, action=a, parent=node)
-            node.add_child(child)
-
-
-def back_propagation(node):
-    """
-    回溯并记录节点路径
-    :param node: 待回溯节点
-    :return: 回溯路径
-    """
-    path = []
-    while node.parent is not None:
-        path.insert(0, node.to_this_action)
-        node = node.parent
-    return path
-
-
-def breadth_first_search(maze):
-    """
-    对迷宫进行广度优先搜索
-    :param maze: 待搜索的maze对象
-    """
-    start = maze.sense_robot()
-    root = SearchTree(loc=start)
-    queue = [root]  # 节点队列，用于层次遍历
-    h, w, _ = maze.maze_data.shape
-    is_visit_m = np.zeros((h, w), dtype=np.int32)  # 标记迷宫的各个位置是否被访问过
-    path = []  # 记录路径
-    while True:
-        current_node = queue[0]
-        is_visit_m[current_node.loc] = 1  # 标记当前节点位置已访问
-
-        if current_node.loc == maze.destination:  # 到达目标点
-            path = back_propagation(current_node)
-            break
-
-        if current_node.is_leaf():
-            expand(maze, is_visit_m, current_node)
-
-        # 入队
-        for child in current_node.children:
-            queue.append(child)
-
-        # 出队
-        queue.pop(0)
-
-    return path
 
 def my_search(maze):
     """
@@ -251,19 +163,6 @@ class Robot(QRobot):
     def _update_target_nn(self):
         self.target_nn.load_state_dict(self.eval_nn.state_dict())
         return
-
-    def _select_action(self, cur_state):
-        state = torch.from_numpy(np.array(cur_state)).float().to(self.device)
-
-        if random.random() < self.epsilon:
-            action = random.choice(self.valid_action)
-        else:
-            self.eval_nn.eval()
-            with torch.no_grad():
-                q_next = self.eval_nn(cur_state).cpu().data.numpy()
-            self.eval_nn.train()
-        
-        return self.valid_action[np.argmin(q_next).item()]
 
     def _get_reward(self, action):
         return self.maze.move_robot(action)
